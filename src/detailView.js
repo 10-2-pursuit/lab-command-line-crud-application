59fd703ba192327806c2cf5f99ab822aea41b76e
Sung Yi <sy@Sungs-Air.fios-router.home>
2023-06-05 21:12:28 -0400

    update detailview

--- src/detailView.js
+++ src/detailView.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 
 function detailView(data = d){
     for(let str of data){
-        console.log(chalk.cyan(`id \t`) + `${str.id} ` + chalk.cyan(`name \t`) + `${str.name} ` + chalk.cyan(`amount \t`) + `${str.amount} ` + chalk.cyan(`donation \t`) +  chalk.yellow(`${str.donation}`));
+        console.log(chalk.cyan(`id \t`) + `${str.id} `.padEnd(10) + chalk.cyan(`name \t`) + `${str.name} `.padEnd(25) + chalk.cyan(`amount \t`) + `${str.amount} `.padEnd(10) + chalk.cyan(`donation `) +  chalk.yellow(`${str.donation}`.padEnd(10)));
     }
 }
 
