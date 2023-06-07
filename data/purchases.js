f2b9b2ef2be0ff033ae1e2366833ce4df9c99faf
Nugget-Meister <tremainecamacho@gmail.com>
2023-06-07 04:40:26 -0400

    update purchase completed, allows updating of single purchase

--- data/purchases.js
+++ data/purchases.js
@@ -1,21 +1,21 @@
 const purchases = [
     {
-        id: 'qPM6D3agRVKwTIiuITsGn',
+        id: 'first',
         name: 'coffee',
         amount: 2,
-        donation: '0.14'
+        donation: 0.14
     },
     {
-        id: '1dk38agRVKwTIiuITsGn',
+        id: 'second',
         name: 'green tea',
         amount: 1,
-        donation: '0.12'
+        donation: 0.12
     },
     {
-        id: '1dk38agRVKhc92jvTsGn',
+        id: 'third',
         name: 'juice',
         amount: 10,
-        donation: '0.50'
+        donation: 0.50
     }
 ]
 
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const {nanoid} = require("nanoid")
-// const {
-//     readJSON,
-//     writeJSON
-// } = require("./src/readWrite")
+const chalk = require("chalk")
+
+// console.log(chalk.blue("strisng"))
+
+
 const purchases = require("./data/purchases")
 
-function createPurchase (name, amount, donation) {
-    let purchase = {
+function createPurchase (data, name, amount, donation) {
+    data.push({
         'id' : nanoid(),
-        "name": name,
-        "amount": amount,
-        "donation": donation.toFixed(2)
-    }
-    return purchase
+        "name": name || "unidentified item",
+        "amount": Number(amount) || 1, 
+        "donation": Number(donation) || 0
+    })
+    return data
 }
 
 
@@ -22,9 +23,42 @@ function showPurchases(data) {
     })
 }
 
-function showDetailedPurchases(){}
+function showDetailedPurchases(data){
+    for(element of data) {
+        console.log(
+            chalk.blue("id"),chalk.white(element.id),
+            chalk.blue("name"),chalk.white(element.name),
+            chalk.blue("amount"),chalk.white(element.amount),
+            chalk.blue("donation"),chalk.white(element.donation))
+    }
+}
 
-function updatePurchase(){}
+function updatePurchase(data){
+    let held = data.map(item => {
+        return item.id
+    })
+
+    let newArr = process.argv.slice(3).map(x => {
+        return x.split("=")
+    })
+
+    newArr = Object.fromEntries(newArr)
+    foundIndex = held.indexOf(newArr.id)
+
+    if(newArr.id && foundIndex != -1) {
+        //Overwrite relevant data, defaults to previous if not found
+        data[foundIndex] = {
+            id : data[foundIndex].id,
+            name : newArr.name || data[foundIndex].name,
+            amount: newArr.amount || data[foundIndex].amount
+        }
+        console.log("Overwrote data with id of",chalk.italic(newArr.id))
+    } else {
+        console.log(chalk.redBright("ID not found or specified. Please enter correct info."))
+    }
+    
+    return data
+}
 
 function deletePurchase(){}
 
@@ -33,16 +67,18 @@ function showDonationTotal(){}
 function run(){
     switch(process.argv[2]){
         case "create":
-            console.log("create")
+            createPurchase(purchases,process.argv[3],process.argv[4],process.argv[5])
+            console.log(`Created `)
             break;
         case "show":
-            console.log("show")
+            console.log(showPurchases(purchases))
             break;
         case "details":
-            console.log("details")
+            console.log(showDetailedPurchases(purchases))
             break;
         case "update":
-            console.log("update")
+            updatePurchase(purchases)
+            // console.log("update")
             break;
         case "destroy":
             console.log("delete")
