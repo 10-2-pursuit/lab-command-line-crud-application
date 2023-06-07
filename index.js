e3e9c15f4a251d6e75e14ea5c502762e3500b684
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-06-06 21:15:11 -0400

    removed some side notes

--- index.js
+++ index.js
@@ -10,58 +10,50 @@ const {
 
 
 const run = () => {
-    // When our users run one of our npm scripts they will pass an argument 
-            // we need to get a handle on that argument so we can execute the correct action
-                // this argument will always be `prcoess.argv[2]`
+
         const action = process.argv[2];
-        // in the event the user passes a third argument it will always be `process.argv[3]`
-            // our app is designed so this will be an animal
+        
         const purchase = process.argv[3];
-        // in order to get the state of our data we need to invoke our readJSONFile helper
-            // we save this in the variable `animals`
+        
         let purchases = readJSONFile("./data", "purchases-data.json")
-        // we will use this variable later 
-            // to determine wether or not we need to record new information to the data file
+        
         let writeToFile = false;
         let updatedPurchases = [];
-        // switch statement will dictate what we are doing based on the 'action' variable
+        
         switch (action) {
-            // the index route will display all of our data
+            
             case "index" :
                 const allPurchases = index(purchases)
                 console.log(allPurchases);
                 break;   
-            // the create route will create a new record in our data
+            
             case "create" :
-                console.log("CREATE IS FIRING")
                 updatedPurchases = create(purchases, purchase) 
                 writeToFile = true;
                 break;
-            // the show route will display information on one specific record
+            
             case "show" :
                 const foundPurchase = show(purchases, purchase)
                 console.log(foundPurchase)
                 break ;  
-            // update will change the data at a specific record
+            
             case "update" :
-                console.log(purchase,  " %%%%%%% ")
                 updatedPurchases = update(purchases, purchase, process.argv[4]);
                 writeToFile = true;
                 break; 
-            // destroy will delete a record from our data at a specific point
+            
             case "destroy" :
                 updatedPurchases = destroy(purchases, purchase);
                 writeToFile = true;
                 break ;  
-            // score will reduce the data's 'points' value to a number and display it to the user
+            
             case "score" :
                 console.log(score(purchases))
                 break; 
             default :
             console.log("hey there was an error")  
         }
-        // once the nescesarry action has been created 
-            // we determine whether or not to write new data to the data file
+        
         if (writeToFile) {
             console.log("new data detected - updating")
             // here we pass our `updatedPurchases` to our writeJSONFile to record out new entries
@@ -70,5 +62,4 @@ const run = () => {
     
     }
     
-    // executing our application
     run()
--- src/purchases-controller.js
+++ src/purchases-controller.js
@@ -3,75 +3,73 @@ const { nanoid } = require("nanoid");
 const purchaseDonation = require("../data/purchases-donations.json")
 const purchasePrice = require("../data/purchases-price.json")
 const inform = console.log
-// this is where we will create our CRUD logic
 
-// accepts all of our animals from our data and a name from the user
+
+
 function create (purchases, purchaseName) {
     const purchase = { name: purchaseName, 
-        // nanoid gives us a uniqe id for our animals
         id: nanoid(4),
         price: purchasePrice[purchaseName],
         donation: purchaseDonation[purchaseName]
     };
-    // adds to our animals array
+
     purchases.push(purchase);
-    // returns the animals array with the new object
+    
     return purchases
 }
-// returns a string for all of our animals 
+ 
 function index (purchases) {
     return purchases.map((purchase) => purchase.id + " " + purchase.name).join("\n")
 }
-// accepts all of our animals from our data and an ID from the user
+
 function show (purchases, purchaseId) {
-    // finds the matching animal
+    
     const foundPurchase = purchases.find((purchase) => purchase.id === purchaseId) ;
-    // reutrns a formattes string
+    
     return foundPurchase.id + " " + foundPurchase.name + " price: " + foundPurchase.price + " donation: " + foundPurchase.donation
 }
-// accepts all of our animals from our data and an ID from the user
+
 function destroy (purchases, purchaseId) {
-    // finds the matching animal's index in the animals array
+    
    const index =  purchases.findIndex((purchase) => purchase.id === purchaseId);
-    // if a match is found
+    
    if (index > -1) {
-    // removes it from the array if it is fond
+    
     purchases.splice(index, 1);
     console.log("we deleted your purchase");
-    // returns the updated array
+    
     return purchases
    } else {
     console.log("couldn't find a purchase with that id")
    }
 }
-// accepts all of our animals from our data and an ID from the user and a new name for the animal to update
+
 function update (purchases, purchaseId, updatedPurchase ) {
-   //  finds the matching animal's index in the animals array
+   
     const index =  purchases.findIndex((purchase) => purchase.id === purchaseId);
-    // if a match is found
+    
     if (index > -1) {
-        // update the id of the animal at the index we found
+        
         purchases[index].id = purchaseId;
-       // update the name of the animal at the index we found
+       
        purchases[index].name = updatedPurchase;
-       // update the price of the animal at the index we found
+       
        purchases[index].price = purchasePrice[updatedPurchase];
-       // update the points of the animal at the index we found
-        // by accessing our `animalPoints` data at the key of our updatedAnimal from the user
+     
         purchases[index].donation = purchaseDonation[updatedPurchase];
        console.log("your purchase has been updated");
-       // return the modified array
-       return purchases
+       
+       return purchases;
    } else {
      console.log("couldn't find a purchase with that id")
     }
    
 }
-// accepts all of our animals data as an array
+
 function score (purchases) {
-    // iterates over the array and combines the donations of each animal
+    
     const total = purchases.reduce((a,b) => (a + b.donation), 0)
-    // returns that number
+    
     return total
 }
 
