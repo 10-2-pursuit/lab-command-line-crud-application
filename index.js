12685cdb5d06b8139ef5844efe0d7d47182499d5
Nicole Slater <nicoleslater@pursuit.org>
2023-06-09 16:16:20 -0400

    Moving along

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {nanoid} = require("nanoid");
 
 function create(id) {
     const customerId = {id: nanoid(4)};
-    customerId.push(id);
+    id.push(customerId);
+    console.log(id)
     return id;
 }
