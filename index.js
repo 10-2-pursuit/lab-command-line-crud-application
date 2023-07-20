cb797f3e71805764f63f5dbdc95f9f1eb36ef7a9
Jeffchua96 <jeffersonchua@pursuit.org>
2023-07-19 22:31:29 -0400

    1s commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const { uptime } = require("os");
+const { readJSONFile, writeJSONFile } = require("./src/helpers");
+const airports = require("airport-codes");
+
+function run (){
+    const action= process.argv[2]
+    console.log(action)
+    const airportCode = process.argv[3]
+    let  airportCodes= readJSONFile("./data", "codes.json",)
+
+    
+
+    let writeToFile = false
+    let updatedAirportCodes = []
+
+    switch (action){
+        case "index":
+    const airportCodes = index(airportCodes)
+        console.log (airportCodes)
+        break;
+    
+    case "create":
+        console.log ("The Airport codes")
+        writeToFile=true
+        break;
+    
+    case "show":
+        const airportCode = show (airportCodes, airportCode)
+        console.log("The Codes")
+        break;
+
+    case "destroy":
+
+    
+    }
+}
+run()
